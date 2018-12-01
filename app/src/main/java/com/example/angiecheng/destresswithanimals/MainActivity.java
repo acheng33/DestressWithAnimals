c06265f004bf7b9b56ef4e60b02262f0058c54ae
Angie Cheng <angiecheng@wirelessprv-10-192-174-139.near.illinois.edu>
2018-12-01 15:02:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/angiecheng/destresswithanimals/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.angiecheng.destresswithanimals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        })
+    }
+}
