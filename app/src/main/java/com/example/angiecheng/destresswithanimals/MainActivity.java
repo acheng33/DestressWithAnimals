639258a389bdfa3406dd4c308fc9be4f0ed5e408
acheng33 <40620815+acheng33@users.noreply.github.com>
2018-12-02 17:21:51 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/angiecheng/destresswithanimals/MainActivity.java
+++ app/src/main/java/com/example/angiecheng/destresswithanimals/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.angiecheng.destresswithanimals;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,24 @@ public class MainActivity extends AppCompatActivity {
         Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                Log.d("start", "worked!");
+            }
+        });
 
+        Button scrollBack = (Button) findViewById(R.id.scrollBack);
+        scrollBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("previous", "I'm working!");
+            }
+        });
+
+        Button scrollForward = (Button) findViewById(R.id.scrollForward);
+        scrollForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("next","I'm also working!");
             }
         });
     }
