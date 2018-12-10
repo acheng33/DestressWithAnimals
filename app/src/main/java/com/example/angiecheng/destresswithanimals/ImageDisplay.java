c0402c899d8ab1d7b50872494a314fe6217933c7
angiecheng <angie.cheng33@gmail.com>
2018-12-09 19:52:02 -0600

    changed the song, set it to the place with slide show

--- app/src/main/java/com/example/angiecheng/destresswithanimals/ImageDisplay.java
+++ app/src/main/java/com/example/angiecheng/destresswithanimals/ImageDisplay.java
@@ -1,15 +1,16 @@
 package com.example.angiecheng.destresswithanimals;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class ImageDisplay extends AppCompatActivity {
     int drawableCounter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,9 @@ public class ImageDisplay extends AppCompatActivity {
             @Override
             public void onClick(View goBackOnePicture) {
                 drawableCounter--;
+                if (drawableCounter < 1) {
+                    drawableCounter = 20;
+                }
                 int previousPhotoID = getBaseContext().getResources().getIdentifier("test" + drawableCounter,
                         "drawable", getBaseContext().getPackageName());
                 ImageView testDisplayPrevious = (ImageView) findViewById(R.id.displayPhoto);
@@ -33,12 +37,18 @@ public class ImageDisplay extends AppCompatActivity {
             @Override
             public void onClick(View goForwardOnePicture) {
                 drawableCounter++;
+                if (drawableCounter > 20) {
+                    drawableCounter = drawableCounter % 20;
+                }
                 ImageView testDisplayNext = (ImageView) findViewById(R.id.displayPhoto);
                 int nextPhotoID = getBaseContext().getResources().getIdentifier("test" + drawableCounter,
                         "drawable", getBaseContext().getPackageName());
                 testDisplayNext.setImageResource(nextPhotoID);
             }
         });
+
+        MediaPlayer playMusic = MediaPlayer.create(ImageDisplay.this, R.raw.song3);
+        playMusic.start();
 
         final Button returnHome = (Button) findViewById(R.id.returnHome);
         returnHome.setOnClickListener(new View.OnClickListener() {
@@ -47,19 +57,13 @@ public class ImageDisplay extends AppCompatActivity {
                 if (returnToHomeScreen == returnHome) {
                     Intent returnHome = new Intent(ImageDisplay.this, MainActivity.class);
                     ImageDisplay.this.startActivity(returnHome);
-                    Log.d("return home", "returned to home screen");
                 }
-                Log.d("returnHome", "worked!");
+                MediaPlayer playMusic = MediaPlayer.create(ImageDisplay.this, R.raw.song3);
+                playMusic.stop();
             }
         });
 
-        final Button favoritePicture = (Button) findViewById(R.id.favoritePicture);
-        favoritePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View favoritePictureList) {
-                Log.d("favoritePicture", "picutre is favorited!");
-            }
-        });
+
 
     }
 }
--- app/src/main/java/com/example/angiecheng/destresswithanimals/MainActivity.java
+++ app/src/main/java/com/example/angiecheng/destresswithanimals/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.angiecheng.destresswithanimals;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -26,77 +25,9 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("start", "worked!");
                 }
                 Log.d("start", "taken to next page!");
-                MediaPlayer testPlayBack = MediaPlayer.create(MainActivity.this, R.raw.song2);
-                testPlayBack.start();
             }
         });
 
-        final Button dogList = (Button) findViewById(R.id.dogList);
-        dogList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View dogPictures) {
-                Log.d("dogList", "dog checked!");
-            }
-        });
-
-        final Button catList = (Button) findViewById(R.id.catList);
-        catList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View catPictures) {
-                Log.d("catList", "cat checked!");
-            }
-        });
-
-        final Button aquaticList = (Button) findViewById(R.id.aquaticList);
-        aquaticList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View aquaticAnimalPictures) {
-                Log.d("aquaticList", "aquatics checked!");
-            }
-        });
-
-        final Button babyAnimalList = (Button) findViewById(R.id.babyAnimalList);
-        babyAnimalList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View babyAnimalPictures) {
-                Log.d("babyAnimalList", "baby animals checked!");
-            }
-        });
-
-        final Button forestAnimalList = (Button) findViewById(R.id.forestAnimalList);
-        forestAnimalList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View forestAnimalPictures) {
-                Log.d("forestAnimalList", "forest animals checked!");
-            }
-        });
-
-        final Button smallAnimalList = (Button) findViewById(R.id.smallAnimalList);
-        smallAnimalList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View smallAnimalPictures) {
-                Log.d("smallAnimalList", "small animals checked!");
-            }
-        });
-
-        final Button horseList = (Button) findViewById(R.id.horseList);
-        horseList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View horsePictures) {
-                Log.d("horseList", "horse list checked!");
-            }
-        });
-
-        final Button foxList = (Button) findViewById(R.id.foxList);
-        foxList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View foxPictures) {
-                Log.d("foxList", "fox list checked!");
-            }
-        });
-
-        MediaPlayer testPlayBack = MediaPlayer.create(MainActivity.this, R.raw.song2);
-        testPlayBack.start();
 
     }
 }
